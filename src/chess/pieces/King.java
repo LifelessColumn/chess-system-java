12c221c22beeaf760480f8f442fe944dbb864641
Alexander Carvalho <alexandersilva193@gmail.com>
2021-03-03 17:15:30 -0300

    Pawn

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -11,11 +11,6 @@ public class King extends ChessPiece{
 		super(board, color);
 	}
 	
-	@Override
-	public String toString() {
-		return "K";
-	}
-	
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
@@ -68,5 +63,10 @@ public class King extends ChessPiece{
 			mat[p.getRow()][p.getColumn()] = true;
 		}
 		return mat;
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
 	}
 }
